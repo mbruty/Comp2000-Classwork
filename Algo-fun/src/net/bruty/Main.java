1a3cb59e898a744f20193857f7a28116bef5f522
Michael Bruty <57625243+mbruty@users.noreply.github.com>
2020-10-03 14:35:41 +0100

    :tada: Created Algo-fun

---
+++ Algo-fun/src/net/bruty/Main.java
@@ -0,0 +1,92 @@
+package net.bruty;
+
+
+import java.util.List;
+import java.util.Random;
+
+import net.bruty.BST.BST;
+import net.bruty.sorting.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] data = createArray(1000000);
+        int n = data.length;
+        long startTime = System.nanoTime();
+        Radix.radixsort(data, n);
+        long endTime = System.nanoTime();
+
+        long duration = (endTime - startTime) / 1000000  ;
+        System.out.println("Runtime duration: \t" + duration + "ms");
+        System.out.println(checkArray(data));
+
+        BST myBST = new BST(10);
+        int[] bstValues = createArray(20);
+        for(int i: bstValues){
+            myBST.insert(i);
+        }
+        myBST.remove(10, myBST);
+        System.out.println(myBST.contains(15));
+        System.out.println(BST.findClosestValue(myBST, 3));
+        System.out.println(BST.validate(myBST));
+
+        System.out.println("\t*** In order Traversal ***");
+        List<Integer> BSTArr =  myBST.inOrderTraversal();
+        for(int i: BSTArr){
+            System.out.println(i);
+        }
+        System.out.println("\t*** Pre order Traversal ***");
+        BSTArr = myBST.preOrderTraversal();
+        for(int i: BSTArr){
+            System.out.println(i);
+        }
+
+        System.out.println("\t*** Post order Traversal ***");
+        BSTArr = myBST.postOrderTraversal();
+        for(int i: BSTArr){
+            System.out.println(i);
+        }
+
+        System.out.println("\t*** Min height ***");
+        myBST.toMinHeight();
+        myBST.printTree();
+
+        System.out.println("\t*** Comparing Trees ***");
+        // Creating a new bst with the original values, but will have a different structure
+        // Because myBst is now changed to a min height bst
+        BST newBST = new BST(10);
+
+        for(int i: bstValues){
+            newBST.insert(i);
+        }
+
+        System.out.println(myBST.equals(newBST));
+
+        // Creating a new bst with different values
+        newBST = new BST(10);
+        bstValues = createArray(20);
+        for(int i: bstValues){
+            newBST.insert(i);
+        }
+
+        System.out.println(myBST.equals(newBST));
+    }
+
+    private static int[] createArray(int size){
+        int[] output = new int[size];
+        Random rand = new Random();
+        for (int i = 0; i < size; i++){
+            output[i] = rand.nextInt(Integer.MAX_VALUE);
+        }
+        return output;
+    }
+
+    private static Boolean checkArray(int[] array){
+        for(int i = 1; i < array.length; i++){
+            if(i > array[i - 1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
