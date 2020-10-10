712675b54f3bc6a5052dcaf29a0151d68a849baa
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2020-10-10 15:54:02 +0100

    :white_check_mark: Added more tests

--- src/test/java/net/bruty/algofun/arrays/ApartmentHuntingTest.java
+++ src/test/java/net/bruty/algofun/arrays/ApartmentHuntingTest.java
@@ -12,6 +12,7 @@ class ApartmentHuntingTest {
     @org.junit.jupiter.api.Test
     void find() {
 
+        // Test One
         List<Map<String, Boolean>> blocks = new ArrayList<>();
         blocks.add(craftMap(false, true, false));
         blocks.add(craftMap(true, false, false));
@@ -19,6 +20,16 @@ class ApartmentHuntingTest {
         blocks.add(craftMap(false, true, false));
         blocks.add(craftMap(false, true, true));
         assertEquals(ApartmentHunting.find(blocks, new String[] { "gym", "school", "store"}), 3);
+
+        // Test Two
+        blocks = new ArrayList<>();
+        blocks.add(craftMap(false, true, true, false));
+        blocks.add(craftMap(true, false, false, false));
+        blocks.add(craftMap(true, false, true, false));
+        blocks.add(craftMap(false, false, true, false));
+        blocks.add(craftMap(false, false, true, false));
+        blocks.add(craftMap(false, false, true, true));
+        assertEquals(ApartmentHunting.find(blocks, new String[] { "gym", "office", "school", "store"}), 2);
 
     }
 
@@ -29,4 +40,14 @@ class ApartmentHuntingTest {
         temp.put("store", store);
         return temp;
     }
+
+    private static Map<String, Boolean> craftMap(boolean gym, boolean office, boolean school, boolean store){
+        Map<String, Boolean> temp = new HashMap<>();
+        temp.put("gym", gym);
+        temp.put("school", school);
+        temp.put("store", store);
+        temp.put("office", office);
+        return temp;
+    }
+
 }
